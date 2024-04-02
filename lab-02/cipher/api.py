32873e137ccdf6658cfb72d598be30a9142c90f1
Khuong Duy <khuongduy0608@gmail.com>
2024-04-02 16:21:32 +0700

    [add] lab-02 vigenere

--- lab-02/cipher/api.py
+++ lab-02/cipher/api.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
-from cipher.caesar import CaesarCipher
+from caesar.caesar_cipher import CaesarCipher
+from vigenere import VigenereCipher
 app = Flask(__name__)
 
 #CAESAR CIPHER ALGORITHM
@@ -21,6 +22,25 @@ def caesar_decrypt():
     key = int(data['key'])
     decypted_text = caesar_cipher.decrypt_text(cipher_text, key)
     return jsonify({'decrypted_message': decypted_text})
+
+#VIGENERE CIPHER ALGORITHM
+vigenere_cipher = VigenereCipher()
+
+@app.route('/api/vigenere/encrypt', methods = ['POST'])
+def vigenere_encrypted():
+    data = request.json
+    plain_text = data['plain_text']
+    key = data['key']
+    encrypted_text = vigenere_cipher.vigenere_encrypt(plain_text, key)
+    return jsonify({'encrypted_text': encrypted_text})
+
+@app.route('/api/vigenere/decrypt', methods = ['POST'])
+def vigenere_decrypted():
+    data = request.json
+    cipher_text = data['cipher_text']
+    key = data['key']
+    decrypted_text = vigenere_cipher.vigenere_decrypt(cipher_text, key)
+    return jsonify({'decrypted_text': decrypted_text})
 
 #main function
 if __name__ == "__main__":
